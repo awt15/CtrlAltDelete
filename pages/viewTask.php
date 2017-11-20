4f813e3288543c9e2bba6bda23b67491f30653e4
awt15 <awt15@my.fsu.edu>
2017-11-19 22:51:11 -0500

    Added information for the header in viewTask
    Updated information in viewTask to have a better header.

--- pages/viewTask.php
+++ pages/viewTask.php
@@ -101,7 +101,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Task ID - Task Title*</h1>
+                    <h1 class="page-header">Project/TaskID: Task Title*</h1>
                 </div>
             </div>
             <div class="row">
