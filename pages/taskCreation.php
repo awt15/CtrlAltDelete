fd61b46ca03a156426dbe0678715a78e294e0bf5
Vita Tran <vita.tran@live.com>
2017-12-02 17:32:03 -0500

    Check if Task Creation is inserting into Changes Table
    For teammates to check if function is working on their computers.

--- pages/taskCreation.php
+++ pages/taskCreation.php
@@ -6,6 +6,7 @@
         exit;
     }
     
+    date_default_timezone_set('America/New_York');
     $blank = "";
     if($_POST['projectID'] != "" && $_POST['dueDate']!="" && $_POST['priority'] != "" && $_POST['title'] != "" && $_POST['assignee'] != "" && $_POST['taskDescript'])
     {
@@ -38,14 +39,23 @@
                 if ($row[0] == 1)
                 {
                     $checkAss = mysqli_query($connection, "SELECT permissions FROM belongto WHERE username='$assignee' AND projectID='$project'");
+
                     if(mysqli_num_rows($checkAss) != 0)
                     {
                         $insert = mysqli_query($connection, "INSERT INTO tasks (dueDate, username, projectID, taskDescription, priority, status, title, abbreviation) VALUES ('$duedate', '$assignee', $project, '$taskDescript', '$priority', 1, '$title', '$abr')");
+                        $tid = mysqli_query($connection, "SELECT taskID FROM tasks WHERE username = '$user' AND projectID = '$project' AND title = '$title' AND taskDescription = '$taskDescript'");
+                        $tidrow = mysqli_fetch_row($tid);
+                        $task = $tidrow[0];
+
                         if ($insert)
                         {
                             $getname = mysqli_query($connection, "SELECT projectName FROM projects WHERE projectID=$project");
                             $res = mysqli_fetch_row($getname);
                             $pname = $res[0];
+
+                            $today = date("Y-m-d H:i:s");
+                            $change = mysqli_query($connection, "INSERT INTO changes (changeID, changeType, projectID, taskID, timestamp, username) VALUES (NULL, 2, '$project', '$task', '$today', '$user')");
+
                             header("Location: viewProject.php?var=".urlencode($pname));
                             exit;
                         }
