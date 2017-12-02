ba1e655c73f63e1ca48ee5dd1a517821c8ec80f6
Mark Walker <mtw14@my.fsu.edu>
2017-12-02 17:45:23 -0500

    Changing task status now properly adds an entry into changes

--- pages/changeStatus.php
+++ pages/changeStatus.php
@@ -13,6 +13,7 @@
             die();
         }
     else{
+        $user = $_SESSION['username'];
     	$status = $_GET['stat'];
     	$tid = $_GET['var'];
     	$pidList = mysqli_query($connection, "SELECT projectID FROM tasks WHERE taskID = '$tid'");
@@ -21,11 +22,14 @@
     	$findProjectInfo = mysqli_query($connection, "SELECT projectName FROM projects WHERE projectID = '$pid'");
         $projectRow = mysqli_fetch_row($findProjectInfo);
         $pname = $projectRow[0];
+        $stamp = date("Y-m-d H:i:s");
     	if ($status == 1){
     		$sql = mysqli_query($connection, "UPDATE tasks SET status = 2 WHERE taskID = '$tid'");
+            $change = mysqli_query($connection, "INSERT INTO changes (changeID, projectID, taskID, timestamp, changetype, username) VALUES (NULL, $pid, $tid, '$stamp', 3, '$user')");
     	}
     	else if ($status == 2){
     		$sql = mysqli_query($connection, "UPDATE tasks SET status = 3 WHERE taskID = '$tid'");
+            $change = mysqli_query($connection, "INSERT INTO changes (changeID, projectID, taskID, timestamp, changetype, username) VALUES (NULL, $pid, $tid, '$stamp', 4, '$user')");
     	}
 
     }
